87e49a164625f91c7ef46f07e39b3c28a5c77d90
Cameron Hyde <chyde@neoformit.com>
2023-10-31 22:57:28 +1000

    Flask API with tests

--- app/app.py
+++ app/app.py
@@ -1,38 +1,9 @@
-"""Basic Flask app."""
+"""Create app."""
 
-import csv
-import os
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from pathlib import Path
-from wtforms import StringField, SubmitField, IntegerField
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
-OUTFILE = Path(os.environ['FLASK_OUTPUT_FILE'])
 
-
-class MyForm(FlaskForm):
-    name = StringField('Name')
-    age = IntegerField('Age')
-    submit = SubmitField('Submit')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = MyForm()
-    if request.method == 'POST':
-        form = MyForm(request.form)
-        if form.validate_on_submit():
-            name = form.name.data
-            age = form.age.data
-            with open(OUTFILE, mode="w", newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(["Name", "Age"])
-                writer.writerow([name, age])
-            return render_template('success.html')
-    return render_template('index.html', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+# Enable CORS to allow requests from client on different origin
+CORS(app, resources={r'/*': {'origins': '*'}})
